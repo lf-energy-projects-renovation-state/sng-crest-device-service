f84e7ac9e254fdd615c5291b524f015abb93c8e4
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-10-09 09:22:23 +0200

    FDP-1623: added licesing info
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/adapter/CoapMessageAdapter.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/adapter/CoapMessageAdapter.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.gxf.crestdeviceservice.adapter
 
 import com.fasterxml.jackson.databind.JsonNode
--- components/kafka/src/main/kotlin/org/gxf/crestdeviceservice/kafka/port/MessageConsumer.kt
+++ components/kafka/src/main/kotlin/org/gxf/crestdeviceservice/kafka/port/MessageConsumer.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.gxf.crestdeviceservice.kafka.port
 
 interface MessageConsumer<T> {
