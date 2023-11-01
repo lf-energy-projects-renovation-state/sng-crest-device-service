54b3aae5fc6688d47bb700f72b3816f4ffd9af89
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-11-01 14:05:44 +0100

    FDP-1687: removed scheduling annotation because we don't even use scheduling
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/CrestDeviceServiceApplication.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/CrestDeviceServiceApplication.kt
@@ -7,10 +7,8 @@ package org.gxf.crestdeviceservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @ConfigurationPropertiesScan
-@EnableScheduling
 @SpringBootApplication
 class CrestDeviceServiceApplication
 
