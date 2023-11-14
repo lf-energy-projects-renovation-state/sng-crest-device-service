cf15d31ec5cf03e7091d906d1dba47b34fe3c2b9
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-11-14 11:50:03 +0100

    FDP-1628: fix test
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/test/kotlin/DatabaseFieldEncryptorTest.kt
+++ application/src/test/kotlin/DatabaseFieldEncryptorTest.kt
@@ -7,18 +7,10 @@ class DatabaseFieldEncryptorTest {
     private val databaseFieldEncryptor = DatabaseFieldEncryptor().apply { secret = "super-secret-key" }
 
     @Test
-    fun shouldEncryptInputData() {
-        val expected = "8fmtjU1emOIC+Rn9Dv/7ppdmH14q9p/jWsNI+xI9xEzvQJNgp1Y6H0y3wonsw/0Z"
-        val result = databaseFieldEncryptor.convertToDatabaseColumn("data")
-
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun shouldDecryptData() {
+    fun shouldBeAbleTeEncryptAndDecryptData() {
         val expected = "data"
-        val result = databaseFieldEncryptor.convertToEntityAttribute("pq+bgx2cDcgGCYikWQnJ7g==")
-
-        assertEquals(expected, result)
+        val encrypted = databaseFieldEncryptor.convertToDatabaseColumn(expected)
+        val decrypted = databaseFieldEncryptor.convertToEntityAttribute(encrypted)
+        assertEquals(expected, decrypted)
     }
 }
