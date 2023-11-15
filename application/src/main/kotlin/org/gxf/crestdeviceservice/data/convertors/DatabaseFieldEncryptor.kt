f91c5149a4e77a0f7d0f0743fbfcfb07d0769fbc
Luciano Favoroso <luciano.favoroso@alliander.com>
2023-11-15 09:35:01 +0100

    FDP-1628: fix encryption
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/data/convertors/DatabaseFieldEncryptor.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/data/convertors/DatabaseFieldEncryptor.kt
@@ -3,18 +3,22 @@ package org.gxf.crestdeviceservice.data.convertors
 import jakarta.persistence.AttributeConverter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.security.MessageDigest
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
 import java.security.SecureRandom
+import java.security.spec.AlgorithmParameterSpec
 import java.util.*
 import javax.crypto.Cipher
-import javax.crypto.spec.IvParameterSpec
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.SecretKeySpec
+
 
 @Component
 class DatabaseFieldEncryptor : AttributeConverter<String, String> {
 
     companion object {
-        private const val ENCRYPTION_METHOD = "AES/CBC/PKCS5Padding"
+        private const val ENCRYPTION_METHOD = "AES/GCM/NoPadding"
         private const val ENCRYPTION_ALGORITHM = "AES"
         private const val IV_SIZE = 16
     }
@@ -23,66 +27,32 @@ class DatabaseFieldEncryptor : AttributeConverter<String, String> {
     lateinit var secret: String
 
     override fun convertToDatabaseColumn(attribute: String): String {
-        return Base64.getEncoder().encodeToString(encrypt(attribute, secret))
+        return Base64.getEncoder().encodeToString(encrypt(attribute, SecretKeySpec(secret.toByteArray(), ENCRYPTION_ALGORITHM)))
     }
 
     override fun convertToEntityAttribute(dbData: String): String {
-        return decrypt(Base64.getDecoder().decode(dbData), secret)
+        return decrypt(Base64.getDecoder().decode(dbData), SecretKeySpec(secret.toByteArray(), ENCRYPTION_ALGORITHM))
     }
 
-    fun encrypt(plainText: String, key: String): ByteArray {
-        val inputDataAsBytes = plainText.toByteArray()
-
-        // Generating IV
-        val iv = ByteArray(IV_SIZE).apply {
-            SecureRandom().nextBytes(this)
-        }
-        val ivParameterSpec = IvParameterSpec(iv)
-
-        // Hashing key
-        val digest: MessageDigest = MessageDigest.getInstance("SHA-256")
-        digest.update(key.toByteArray(charset("UTF-8")))
-        val keyBytes = ByteArray(IV_SIZE)
-        System.arraycopy(digest.digest(), 0, keyBytes, 0, keyBytes.size)
-        val secretKeySpec = SecretKeySpec(keyBytes, ENCRYPTION_ALGORITHM)
-
-        // Encrypt
+    private fun encrypt(plaintext: String, secretKey: SecretKey): ByteArray {
+        val iv = ByteArray(IV_SIZE)
+        val secureRandom = SecureRandom()
+        secureRandom.nextBytes(iv)
         val cipher = Cipher.getInstance(ENCRYPTION_METHOD)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec)
-        val encrypted = cipher.doFinal(inputDataAsBytes)
-
-        // Combine IV and encrypted part
-        val encryptedIVAndText = ByteArray(IV_SIZE + encrypted.size)
-        System.arraycopy(iv, 0, encryptedIVAndText, 0, IV_SIZE)
-        System.arraycopy(encrypted, 0, encryptedIVAndText, IV_SIZE, encrypted.size)
-        return encryptedIVAndText
+        val parameterSpec = GCMParameterSpec(128, iv)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec)
+        val cipherText = cipher.doFinal(plaintext.toByteArray(StandardCharsets.UTF_8))
+        val byteBuffer: ByteBuffer = ByteBuffer.allocate(iv.size + cipherText.size)
+        byteBuffer.put(iv)
+        byteBuffer.put(cipherText)
+        return byteBuffer.array()
     }
 
-    fun decrypt(encryptedIvTextBytes: ByteArray, key: String): String {
-        val ivSize = IV_SIZE
-        val keySize = secret.length
-
-        // Extract IV
-        val iv = ByteArray(ivSize)
-        System.arraycopy(encryptedIvTextBytes, 0, iv, 0, iv.size)
-        val ivParameterSpec = IvParameterSpec(iv)
-
-        // Extract encrypted part
-        val encryptedSize = encryptedIvTextBytes.size - ivSize
-        val encryptedBytes = ByteArray(encryptedSize)
-        System.arraycopy(encryptedIvTextBytes, ivSize, encryptedBytes, 0, encryptedSize)
-
-        // Hash key
-        val keyBytes = ByteArray(keySize)
-        val md: MessageDigest = MessageDigest.getInstance("SHA-256")
-        md.update(key.toByteArray())
-        System.arraycopy(md.digest(), 0, keyBytes, 0, keyBytes.size)
-        val secretKeySpec = SecretKeySpec(keyBytes, ENCRYPTION_ALGORITHM)
-
-        // Decrypt
-        val cipherDecrypt = Cipher.getInstance(ENCRYPTION_METHOD)
-        cipherDecrypt.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec)
-        val decrypted = cipherDecrypt.doFinal(encryptedBytes)
-        return String(decrypted)
+    private fun decrypt(cipherMessage: ByteArray, secretKey: SecretKey): String {
+        val cipher = Cipher.getInstance(ENCRYPTION_METHOD)
+        val gcmIv: AlgorithmParameterSpec = GCMParameterSpec(128, cipherMessage, 0, IV_SIZE)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmIv)
+        val plainText = cipher.doFinal(cipherMessage, IV_SIZE, cipherMessage.size - IV_SIZE)
+        return String(plainText, StandardCharsets.UTF_8)
     }
 }
