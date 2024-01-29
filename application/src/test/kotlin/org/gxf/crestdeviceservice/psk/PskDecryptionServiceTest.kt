d482fc713146462ed5dd359ee3144d5d4a6500cc
Jasper Kamerling <jasper.kamerling@alliander.com>
2024-01-29 13:09:34 +0100

    FDP-1736: Add decryption service test
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

---
+++ application/src/test/kotlin/org/gxf/crestdeviceservice/psk/PskDecryptionServiceTest.kt
@@ -0,0 +1,50 @@
+package org.gxf.crestdeviceservice.psk
+
+import org.assertj.core.api.Assertions.assertThat
+import org.gxf.crestdeviceservice.psk.configuration.PskDecryptionConfiguration
+import org.junit.jupiter.api.Test
+
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.PublicKey
+import java.security.interfaces.RSAPrivateKey
+import java.util.Base64
+import javax.crypto.Cipher
+
+class PskDecryptionServiceTest {
+
+    @Test
+    fun decryptSecret() {
+        val keyRef = "1"
+        val secret = "12345"
+
+        // Create keys and encrypt the test secret
+        val keyPair = generateKeyPair()
+        val encryptedSecret = createSecret(secret, keyPair.public)
+
+        // Create the testing decryption service
+        val decryptionService = PskDecryptionService(
+            PskDecryptionConfiguration(
+                mapOf(keyRef to keyPair.private as RSAPrivateKey)
+            )
+        )
+
+        // Decrypt the secret
+        val decryptedSecret = decryptionService.decryptSecret(encryptedSecret, keyRef)
+
+        assertThat(decryptedSecret).isEqualTo(secret)
+    }
+
+    private fun generateKeyPair(): KeyPair =
+        KeyPairGenerator.getInstance("RSA")
+            .apply { initialize(4096) }
+            .generateKeyPair()
+
+    private fun createSecret(secret: String, publicKey: PublicKey) =
+        Cipher.getInstance("RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING")
+            .apply { init(Cipher.ENCRYPT_MODE, publicKey) }
+            .doFinal(secret.toByteArray())
+            .let { Base64.getEncoder().encodeToString(it) }
+
+
+}
