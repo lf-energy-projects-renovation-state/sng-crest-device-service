d2ebf2cb6a549a8f0fe76cfd0253461bcc2a0b66
Jasper Kamerling <jasper.kamerling@alliander.com>
2024-01-29 11:20:26 +0100

    FDP-1736: Use private key for decryption
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/psk/configuration/PskDecryptionConfiguration.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/psk/configuration/PskDecryptionConfiguration.kt
@@ -2,7 +2,6 @@ package org.gxf.crestdeviceservice.psk.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import java.security.interfaces.RSAPrivateKey
-import java.security.interfaces.RSAPublicKey
 
 @ConfigurationProperties("crest-device-service.psk.decryption")
-class PskDecryptionConfiguration(val privateKey: Map<String, RSAPrivateKey>, val publicKey: Map<String, RSAPublicKey>)
+class PskDecryptionConfiguration(val privateKey: Map<String, RSAPrivateKey>)
