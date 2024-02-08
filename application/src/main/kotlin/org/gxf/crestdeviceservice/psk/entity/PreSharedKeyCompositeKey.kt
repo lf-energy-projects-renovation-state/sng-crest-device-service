0b2b4ed75c6e4caa5894ac6be474d98139c8a2b9
Jasper Kamerling <jasper.kamerling@alliander.com>
2024-02-08 13:16:46 +0100

    FDP-1919: Remove unused import
    Signed-off-by: Jasper Kamerling <jasper.kamerling@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/psk/entity/PreSharedKeyCompositeKey.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/psk/entity/PreSharedKeyCompositeKey.kt
@@ -1,7 +1,6 @@
 package org.gxf.crestdeviceservice.psk.entity
 
 import java.io.Serializable
-import java.time.Instant
 
 class PreSharedKeyCompositeKey(val identity: String?,
                                val version: Int?) : Serializable {
