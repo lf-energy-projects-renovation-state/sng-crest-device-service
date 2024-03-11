c715bbdd34cb0e9087e64a15e8be24709b3bbd53
Luciano Favoroso <luciano.favoroso@alliander.com>
2024-03-11 14:38:14 +0100

    FDP-1884: Made method variable
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/psk/configuration/PskDecryptionConfiguration.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/psk/configuration/PskDecryptionConfiguration.kt
@@ -4,4 +4,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import java.security.interfaces.RSAPrivateKey
 
 @ConfigurationProperties("crest-device-service.psk.decryption")
-class PskDecryptionConfiguration(val privateKey: Map<String, RSAPrivateKey>)
+class PskDecryptionConfiguration(val privateKey: Map<String, RSAPrivateKey>, val method: String)
