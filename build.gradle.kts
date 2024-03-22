5985c0f11edca02d975445b1c374566eb98a91e4
Loes Immens <loes.immens@alliander.com>
2024-03-22 16:43:50 +0100

    FDP-1961: update spring boot and kotlin
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ import io.spring.gradle.dependencymanagement.internal.dsl.StandardDependencyMana
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.2" apply false
+    id("org.springframework.boot") version "3.2.3" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
-    kotlin("jvm") version "1.9.22" apply false
+    kotlin("jvm") version "1.9.23" apply false
     kotlin("plugin.spring") version "1.9.22" apply false
     kotlin("plugin.jpa") version "1.9.22" apply false
     id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1" apply false
