6b8942168c3a69c68853edb6b17f5cb9e5ba0cb8
Loes Immens <loes.immens@alliander.com>
2024-03-22 11:49:40 +0100

    FDP-1961: DownlinkServiceTest working unit tests
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/test/kotlin/org/gxf/crestdeviceservice/TestHelper.kt
+++ application/src/test/kotlin/org/gxf/crestdeviceservice/TestHelper.kt
@@ -8,11 +8,35 @@ import com.fasterxml.jackson.databind.node.TextNode
 
 object TestHelper {
     // Returns a JsonNode representation of: ["INIT", {"DL": "0"}]
-    fun unsollicitedResultCode(): ArrayNode {
+    fun unsollicitedResultCodeInit(): ArrayNode {
         val mapper = ObjectMapper()
         val urcList = listOf(
             TextNode("INIT"),
             ObjectNode(JsonNodeFactory.instance, mapOf("DL" to TextNode("0")))
+        )
+        return mapper.valueToTree(urcList)
+    }
+
+    fun unsollicitedResultCodeSuccess(): ArrayNode {
+        val mapper = ObjectMapper()
+        val urcList = listOf(
+            TextNode("PSK:SET"),
+            ObjectNode(
+                JsonNodeFactory.instance,
+                mapOf("DL" to TextNode("!PSK:key:ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cd;PSK:key:ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cdSET"))
+            )
+        )
+        return mapper.valueToTree(urcList)
+    }
+
+    fun unsollicitedResultCodeFailure(): ArrayNode {
+        val mapper = ObjectMapper()
+        val urcList = listOf(
+            TextNode("PSK:EQER"),
+            ObjectNode(
+                JsonNodeFactory.instance,
+                mapOf("DL" to TextNode("!PSK:key:ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cd;PSK:key:ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cdSET"))
+            )
         )
         return mapper.valueToTree(urcList)
     }
--- application/src/test/kotlin/org/gxf/crestdeviceservice/coap/DownlinkServiceTest.kt
+++ application/src/test/kotlin/org/gxf/crestdeviceservice/coap/DownlinkServiceTest.kt
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import java.time.Instant
 
@@ -23,43 +24,64 @@ class DownlinkServiceTest {
     @InjectMocks
     private lateinit var downLinkService: DownlinkService
 
-//    @Test
-//    fun shouldReturnNoActionDownlinkWhenThereIsNoNewPsk() {
-//        whenever(pskService.needsKeyChange(any())).thenReturn(false)
-//        whenever(pskService.getCurrentPskWithStatus(any(), any())).thenReturn()
-//
-//        val urcNode = TestHelper.unsollicitedResultCode()
-//        val result = downLinkService.getDownlinkForIdentity("identity", urcNode)
-//
-//        assertThat(result).isEqualTo("0")
-//    }
-
     @Test
     fun shouldReturnPskDownlinkWhenThereIsANewPsk() {
+        val identity = "identity"
         val expectedKey = "key"
         val expectedHash = "ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cd"
-
-        whenever(pskService.needsKeyChange(any())).thenReturn(true)
-        whenever(pskService.generateAndSetNewKeyForIdentity(any())).thenReturn(
-            PreSharedKey(
-                "identity",
-                0,
-                Instant.now(),
-                expectedKey,
-                "secret",
-                PreSharedKeyStatus.PENDING
-            )
+        val psk = PreSharedKey(
+            identity,
+            1,
+            Instant.now(),
+            expectedKey,
+            "secret",
+            PreSharedKeyStatus.READY
         )
 
-        val urcNode = TestHelper.unsollicitedResultCode()
-        val result = downLinkService.getDownlinkForIdentity("identity", urcNode)
+        whenever(pskService.needsKeyChange(any())).thenReturn(true)
+        whenever(pskService.saveReadyKeyForIdentityAsPending(any())).thenReturn(psk)
+
+        val urcNode = TestHelper.unsollicitedResultCodeInit()
+        val result = downLinkService.getDownlinkForIdentity(identity, urcNode)
 
         // Psk command is formatted as: PSK:[Key]:[Hash];PSK:[Key]:[Hash]SET
+        println("!PSK:$expectedKey:$expectedHash;PSK:$expectedKey:${expectedHash}SET")
         assertThat(result).isEqualTo("!PSK:$expectedKey:$expectedHash;PSK:$expectedKey:${expectedHash}SET")
     }
 
     @Test
-    fun shouldInterpretDl() {
+    fun shouldReturnNoActionDownlinkWhenThereIsNoNewPsk() {
+        whenever(pskService.needsKeyChange(any())).thenReturn(false)
 
+        val urcNode = TestHelper.unsollicitedResultCodeInit()
+        val result = downLinkService.getDownlinkForIdentity("identity", urcNode)
+
+        assertThat(result).isEqualTo("0")
+    }
+
+    @Test
+    fun shouldChangeActiveKeyWhenSuccessURCReceived() {
+        val urcNode = TestHelper.unsollicitedResultCodeSuccess()
+        val identity = "identity"
+
+        whenever(pskService.needsKeyChange(any())).thenReturn(false)
+        whenever(pskService.pendingKeyPresent(any())).thenReturn(true)
+
+        downLinkService.getDownlinkForIdentity(identity, urcNode)
+
+        verify(pskService).changeActiveKey(identity)
+    }
+
+    @Test
+    fun shouldSetPendingKeyAsInvalidWhenFailureURCReceived() {
+        val urcNode = TestHelper.unsollicitedResultCodeFailure()
+        val identity = "identity"
+
+        whenever(pskService.needsKeyChange(any())).thenReturn(false)
+        whenever(pskService.pendingKeyPresent(any())).thenReturn(true)
+
+        downLinkService.getDownlinkForIdentity(identity, urcNode)
+
+        verify(pskService).setPendingKeyAsInvalid(identity)
     }
 }
