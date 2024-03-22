8b42b954930bd94d66f5652d7b436ade59a196ae
Loes Immens <loes.immens@alliander.com>
2024-03-22 14:51:11 +0100

    FDP-1961: copyright headers
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/psk/exception/NoExistingPskException.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/psk/exception/NoExistingPskException.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.gxf.crestdeviceservice.psk.exception
 
 class NoExistingPskException(message: String) : Exception(message)
--- application/src/test/kotlin/org/gxf/crestdeviceservice/TestHelper.kt
+++ application/src/test/kotlin/org/gxf/crestdeviceservice/TestHelper.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.gxf.crestdeviceservice
 
 import com.fasterxml.jackson.databind.ObjectMapper
