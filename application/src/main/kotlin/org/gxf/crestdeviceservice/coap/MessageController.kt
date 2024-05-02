d3986307fd63b89dd755819499c7fe125c9c33dc
Luciano Favoroso <luciano.favoroso@alliander.com>
2024-05-02 16:08:44 +0200

    FDP-2219: Added more logging
    Signed-off-by: Luciano Favoroso <luciano.favoroso@alliander.com>

--- application/src/main/kotlin/org/gxf/crestdeviceservice/coap/MessageController.kt
+++ application/src/main/kotlin/org/gxf/crestdeviceservice/coap/MessageController.kt
@@ -8,18 +8,14 @@ import com.fasterxml.jackson.databind.JsonNode
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.lang.NonNull
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/sng")
 class MessageController(
-    private val messageService: MessageService,
-    private val downlinkService: DownlinkService,
-    private val urcService: UrcService
+        private val messageService: MessageService,
+        private val downlinkService: DownlinkService,
+        private val urcService: UrcService
 ) {
 
     private val logger = KotlinLogging.logger {}
@@ -43,7 +39,7 @@ class MessageController(
                 val downlink = downlinkService.getDownlinkForIdentity(identity, body)
                 return ResponseEntity.ok(downlink)
             } catch (e: Exception) {
-                logger.error { "Exception occurred while creating downlink for device $identity" }
+                logger.error(e) { "Exception occurred while creating downlink for device $identity" }
                 return ResponseEntity.internalServerError().build()
             }
         }
