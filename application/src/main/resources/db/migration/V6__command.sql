41334761ebdf0dcb4b3ca9cc7a5e6fc16ce25d17
Loes Immens <loes.immens@alliander.com>
2024-08-26 16:10:03 +0200

    FDP-2359: correct flyway scripts
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/main/resources/db/migration/V6__command.sql
+++ application/src/main/resources/db/migration/V6__command.sql
@@ -1,10 +1,10 @@
 create table command
 (
     id              uuid            not null,
-    deviceId        varchar(255)    not null,
-    correlationId   uuid            not null,
-    timestampIssued timestamp       not null,
+    device_id        varchar(255)    not null,
+    correlation_id   uuid            not null,
+    timestamp_issued timestamp       not null,
     type            varchar(255)    not null,
-    commandValue    varchar(255)    not null,
+    command_value    varchar(255)    not null,
     status          varchar(255)    not null
 );
---
+++ application/src/main/resources/db/test-data/V7__command_test_data.sql
@@ -0,0 +1,8 @@
+insert into command (id, device_id, correlation_id, timestamp_issued, type, command_value, status)
+values ('33836a60-c1d7-4b32-88c2-3827b0e7c611',
+        '867787050253370',
+        'c9406f63-7247-4206-bd74-12b775124b3e',
+        now(),
+        'REBOOT',
+        'reboot',
+        'PENDING');
