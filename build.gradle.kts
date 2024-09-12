a5d29900e8e6d4c4c20fb2ca4d4efaed50683b0f
Loes Immens <loes.immens@alliander.com>
2024-09-12 11:20:59 +0200

    FDP-2318: added task description and group
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,6 +81,8 @@ subprojects {
     }
 
     tasks.register<Copy>("updateGitHooks") {
+        description = "Copies the pre-commit Git Hook to the .git/hooks folder."
+        group = "verification"
         from("${project.rootDir}/scripts/pre-commit")
         into("${project.rootDir}/.git/hooks")
     }
