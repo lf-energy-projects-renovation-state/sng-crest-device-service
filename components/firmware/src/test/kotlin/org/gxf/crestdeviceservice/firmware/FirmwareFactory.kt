a5e2032d187254d0ec755912194a377f9fa5633a
Sander van der Heijden <sander.van.der.heijden@alliander.com>
2024-11-19 13:58:12 +0100

    FDP-2551 ~ Adds additional firmware mapper unit tests
    Signed-off-by: Sander van der Heijden <sander.van.der.heijden@alliander.com>

---
+++ components/firmware/src/test/kotlin/org/gxf/crestdeviceservice/firmware/FirmwareFactory.kt
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: Copyright Contributors to the GXF project
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.gxf.crestdeviceservice.firmware
+
+import java.util.UUID
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_FROM_VERSION
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_NAME
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_PACKET_0
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_UUID
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_VERSION
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.PREVIOUS_FIRMWARE_UUID
+import org.gxf.crestdeviceservice.firmware.entity.Firmware
+import org.gxf.crestdeviceservice.firmware.entity.FirmwarePacket
+import org.springframework.core.io.ClassPathResource
+import org.springframework.mock.web.MockMultipartFile
+
+object FirmwareFactory {
+
+    fun getFirmwareFile(fileName: String): MockMultipartFile {
+        val firmwareFile = ClassPathResource(fileName).file
+        return MockMultipartFile("file", firmwareFile.name, "text/plain", firmwareFile.readBytes())
+    }
+
+    fun getFirmwareEntity(name: String): Firmware {
+        val firmware = Firmware(FIRMWARE_UUID, name, FIRMWARE_VERSION, PREVIOUS_FIRMWARE_UUID, mutableListOf())
+        val packet = getFirmwarePacket(firmware)
+        firmware.packets.add(packet)
+        return firmware
+    }
+
+    fun getFirmwareEntityWithoutPreviousVersion(name: String): Firmware {
+        val firmware = Firmware(FIRMWARE_UUID, name, FIRMWARE_VERSION, null, mutableListOf())
+        val packet = getFirmwarePacket(firmware)
+        firmware.packets.add(packet)
+        return firmware
+    }
+
+    fun getPreviousFirmwareEntity(): Firmware =
+        Firmware(PREVIOUS_FIRMWARE_UUID, FIRMWARE_NAME, FIRMWARE_FROM_VERSION, UUID.randomUUID(), mutableListOf())
+
+    private fun getFirmwarePacket(firmware: Firmware) = FirmwarePacket(firmware, 0, FIRMWARE_PACKET_0)
+}
--- components/firmware/src/test/kotlin/org/gxf/crestdeviceservice/firmware/mapper/FirmwareMapperTest.kt
+++ components/firmware/src/test/kotlin/org/gxf/crestdeviceservice/firmware/mapper/FirmwareMapperTest.kt
@@ -11,17 +11,24 @@ import io.mockk.junit5.MockKExtension
 import java.util.Optional
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.gxf.crestdeviceservice.FirmwareFactory.getFirmwareEntity
-import org.gxf.crestdeviceservice.FirmwareFactory.getPreviousFirmwareEntity
-import org.gxf.crestdeviceservice.TestConstants.FIRMWARE_FROM_VERSION
-import org.gxf.crestdeviceservice.TestConstants.FIRMWARE_NAME
-import org.gxf.crestdeviceservice.TestConstants.FIRMWARE_VERSION
-import org.gxf.crestdeviceservice.TestConstants.FIRMWARE_WRONG_NAME
-import org.gxf.crestdeviceservice.TestConstants.PREVIOUS_FIRMWARE_UUID
+import org.gxf.crestdeviceservice.firmware.FirmwareFactory.getFirmwareEntity
+import org.gxf.crestdeviceservice.firmware.FirmwareFactory.getFirmwareEntityWithoutPreviousVersion
+import org.gxf.crestdeviceservice.firmware.FirmwareFactory.getFirmwareFile
+import org.gxf.crestdeviceservice.firmware.FirmwareFactory.getPreviousFirmwareEntity
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_FILE_DELTA_NAME
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_FILE_DELTA_PACKET_SIZE
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_FILE_DELTA_PREVIOUS_VERSION
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_FILE_DELTA_VERSION
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_FROM_VERSION
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_NAME
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_VERSION
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.FIRMWARE_WRONG_NAME
+import org.gxf.crestdeviceservice.firmware.FirmwareTestConstants.PREVIOUS_FIRMWARE_UUID
 import org.gxf.crestdeviceservice.firmware.exception.FirmwareException
 import org.gxf.crestdeviceservice.firmware.repository.FirmwareRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.web.multipart.MultipartFile
 
 @ExtendWith(MockKExtension::class)
 class FirmwareMapperTest {
@@ -30,7 +37,40 @@ class FirmwareMapperTest {
     @InjectMockKs private lateinit var firmwareMapper: FirmwareMapper
 
     @Test
-    fun mapEntitiesToFirmwares() {
+    fun shouldMapFirmwareFileToEntity() {
+        // arrange
+        val file: MultipartFile = getFirmwareFile(FIRMWARE_FILE_DELTA_NAME)
+        val previousFirmware = getPreviousFirmwareEntity()
+        every { firmwareRepository.findByVersion(FIRMWARE_FILE_DELTA_PREVIOUS_VERSION) } returns previousFirmware
+
+        // act
+        val firmware = firmwareMapper.mapFirmwareFileToEntity(file)
+
+        // assert
+        assertThat(firmware.name).isEqualTo(FIRMWARE_FILE_DELTA_NAME)
+        assertThat(firmware.version).isEqualTo(FIRMWARE_FILE_DELTA_VERSION)
+        assertThat(firmware.previousFirmwareId).isEqualTo(previousFirmware.id)
+        assertThat(firmware.packets.size).isEqualTo(FIRMWARE_FILE_DELTA_PACKET_SIZE)
+    }
+
+    @Test
+    fun shouldMapFirmwareFileToEntityWhenNoPrevousVersionPresent() {
+        // arrange
+        val file: MultipartFile = getFirmwareFile(FIRMWARE_FILE_DELTA_NAME)
+        every { firmwareRepository.findByVersion(FIRMWARE_FILE_DELTA_PREVIOUS_VERSION) } returns null
+
+        // act
+        val firmware = firmwareMapper.mapFirmwareFileToEntity(file)
+
+        // assert
+        assertThat(firmware.name).isEqualTo(FIRMWARE_FILE_DELTA_NAME)
+        assertThat(firmware.version).isEqualTo(FIRMWARE_FILE_DELTA_VERSION)
+        assertThat(firmware.previousFirmwareId).isNull()
+        assertThat(firmware.packets.size).isEqualTo(FIRMWARE_FILE_DELTA_PACKET_SIZE)
+    }
+
+    @Test
+    fun shouldMapEntitiesToFirmwares() {
         val firmwareEntities = listOf(getFirmwareEntity(FIRMWARE_NAME))
         val previousFirmware = getPreviousFirmwareEntity()
 
@@ -47,7 +87,21 @@ class FirmwareMapperTest {
     }
 
     @Test
-    fun mapEntitiesToFirmwaresThrowsException() {
+    fun shouldMapEntitiesToFirmwaresWhenNoPreviousFirmwarePresent() {
+        val firmwareEntities = listOf(getFirmwareEntityWithoutPreviousVersion(FIRMWARE_NAME))
+
+        val result = firmwareMapper.mapEntitiesToFirmwares(firmwareEntities)
+
+        val firmware = result.firmwares.first()
+        assertThat(firmware.name).isEqualTo(FIRMWARE_NAME)
+        assertThat(firmware.version).isEqualTo(FIRMWARE_VERSION)
+        assertThat(firmware.type).isEqualTo(FirmwareType.device)
+        assertThat(firmware.fromVersion).isNull()
+        assertThat(firmware.numberOfPackages).isEqualTo(1)
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenUsingWrongFirmwareName() {
         val firmwareEntities = listOf(getFirmwareEntity(FIRMWARE_WRONG_NAME))
 
         assertThatThrownBy { firmwareMapper.mapEntitiesToFirmwares(firmwareEntities) }
