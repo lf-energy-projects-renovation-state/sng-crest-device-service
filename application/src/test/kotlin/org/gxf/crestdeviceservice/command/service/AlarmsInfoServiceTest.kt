de6ecf1ae815a477c763b00d9e0fd14a193ecd37
Loes Immens <loes.immens@alliander.com>
2025-01-30 12:15:37 +0100

    FDP-2874: unit test for failing downlinks
    Signed-off-by: Loes Immens <loes.immens@alliander.com>

--- application/src/test/kotlin/org/gxf/crestdeviceservice/command/service/AlarmsInfoServiceTest.kt
+++ application/src/test/kotlin/org/gxf/crestdeviceservice/command/service/AlarmsInfoServiceTest.kt
@@ -5,12 +5,17 @@ package org.gxf.crestdeviceservice.command.service
 
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.junit5.MockKExtension
+import java.util.stream.Stream
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.gxf.crestdeviceservice.MessageFactory
+import org.gxf.crestdeviceservice.TestConstants.ALARMS_INFO
 import org.gxf.crestdeviceservice.TestConstants.ALARMS_INFO_DOWNLINK
-import org.gxf.crestdeviceservice.model.AlarmsInfo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @ExtendWith(MockKExtension::class)
 class AlarmsInfoServiceTest {
@@ -19,11 +24,32 @@ class AlarmsInfoServiceTest {
     @Test
     fun getAlarmsInfo() {
         val message = MessageFactory.messageWithUrc(listOf(), ALARMS_INFO_DOWNLINK)
-        val expected =
-            AlarmsInfo(AL0 = listOf(0, 1, 0, 1, 0), AL1 = listOf(0, 0, 0, 0, 0), AL6 = listOf(100, 200, 300, 400, 10))
+        val expected = ALARMS_INFO
 
         val result = alarmsInfoService.getAlarmsInfo(message)
 
         assertThat(result).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("incorrectDownlinks")
+    fun getAlarmsInfoThrowsException(downlink: String) {
+        val message = MessageFactory.messageWithUrc(listOf(), downlink)
+
+        assertThatThrownBy { alarmsInfoService.getAlarmsInfo(message) }.isInstanceOf(Exception::class.java)
+    }
+
+    companion object {
+        @JvmStatic
+        fun incorrectDownlinks(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(
+                    "\"!INFO:ALARMS\", {\"AL\":[0,1,0,1,0], \"AL1\":[0,0,0,0,0], \"AL6\":[0,500,1000,1500,10]}"
+                ),
+                Arguments.of("{\"AL0\":[0,1,0,1,0], \"AL1\":[0,0,0,0,0], \"AL6\":[0,500,1000,1500,10]}"),
+                Arguments.of("\"!INFO:ALARMS\""),
+                Arguments.of("\"!INFO:ALARMS\", {\"AL0\":0,1,0,1,0, \"AL1\":0,0,0,0,0, \"AL6\":0,500,1000,1500,10}"),
+                Arguments.of("\"!INFO:ALARMS\", \"AL0\":[0,1,0,1,0], \"AL1\":[0,0,0,0,0], \"AL6\":[0,500,1000,1500,10]"),
+            )
     }
 }
