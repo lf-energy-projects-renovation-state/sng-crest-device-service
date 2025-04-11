20f6e89c409c3f7e5a129b8f8e0a5131399c53e2
Sander Verbruggen <sander.verbruggen@alliander.com>
2025-04-11 13:40:16 +0200

    FDP-3125: Added (completed?) unit test for firmware hashing
    Signed-off-by: Sander Verbruggen <sander.verbruggen@alliander.com>

--- components/firmware/src/test/kotlin/org/gxf/crestdeviceservice/firmware/service/FirmwareHashServiceTest.kt
+++ components/firmware/src/test/kotlin/org/gxf/crestdeviceservice/firmware/service/FirmwareHashServiceTest.kt
@@ -3,26 +3,26 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.gxf.crestdeviceservice.firmware.service
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import java.util.UUID
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import org.assertj.core.api.Assertions.assertThat
 import org.gxf.crestdeviceservice.firmware.entity.Firmware
 import org.gxf.crestdeviceservice.firmware.entity.FirmwarePacket
 import org.junit.jupiter.api.Test
 
 class FirmwareHashServiceTest {
-    private val logger = KotlinLogging.logger {}
-    private val fileContent = Path("src/test/resources/firmware-oneliner.txt").readLines()
+    private val originalFirmwarePacket = Path("src/test/resources/firmware-oneliner.txt").readLines().first()
+    private val expectedFirmwarePacket = Path("src/test/resources/expected-firmware-oneliner.txt").readLines().first()
     private val firmwareHashService = FirmwareHashService()
 
     @Test
     fun shouldGenerateFirstPacket() {
         val firmware = Firmware(UUID.randomUUID(), name = "a firmware", version = "1.10")
-        val rawPacket = FirmwarePacket(firmware, 0, fileContent[0])
+        val originalPacket = FirmwarePacket(firmware, 0, originalFirmwarePacket)
 
-        val actualPacket = firmwareHashService.generateDeviceSpecificPacket(rawPacket, "PONMLKJIHGFEDCBA")
+        val actualPacket = firmwareHashService.generateDeviceSpecificPacket(originalPacket, "PONMLKJIHGFEDCBA")
 
-        logger.info { "Resulting packet: $actualPacket" }
+        assertThat(actualPacket).isEqualTo(expectedFirmwarePacket)
     }
 }
